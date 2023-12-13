87d9cb35d21e2a9254122cc152591bf3646da955
jir88 <john.robinson@wustl.edu>
2023-12-13 13:24:31 -0600

    Add map_cd_alignment to extract data table relationships in Compound Discoverer alignment results.

---
+++ R/map_cd_alignment.R
@@ -0,0 +1,60 @@
+#' Map data table relationships in Compound Discoverer alignment results
+#'
+#' Gets the relationships between alignment data tables.
+#'
+#' @details
+#' Compound Discoverer alignment results store data across many tables in an
+#' SQLite database. The database also contains a map of how these tables relate
+#' to each other. This function generates an igraph network from the map.
+#'
+#' @note
+#' Compound Discoverer software is produced by Thermo Fisher Scientific. This
+#' package is not affiliated with Thermo Fisher Scientific in any way. For an
+#' official Python interface to Compound Discoverer alignment files, see
+#' \url{https://github.com/thermofisherlsms/pyeds}
+#'
+#' @param msa An ms_alignment object to query
+#' @param tables Also return the data relationship tables?
+#'
+#' @return An igraph object containing the relationship between data tables. If
+#' tables is TRUE, returns a list containing the graph, a table describing all
+#' data tables, and a table describing relationships between tables.
+#'
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
+#' @export
+map_cd_alignment <- function(msa, tables = FALSE) {
+  # grab the tables
+  data_types <- dplyr::tbl(msa$db_connection, "DataTypes") %>%
+    dplyr::collect()
+  data_connections <- dplyr::tbl(msa$db_connection, "ConnectedDataTypes") %>%
+    dplyr::collect()
+
+  # construct list of relationships
+  df <- data_connections %>%
+    dplyr::left_join(y = dplyr::select(data_types, "DataTypeID", "TableName"),
+              by = c("DataTypeID1" = "DataTypeID")) %>%
+    dplyr::left_join(y = dplyr::select(data_types, "DataTypeID", "TableName"),
+              by = c("DataTypeID2" = "DataTypeID"),
+              suffix = c(".1", ".2")) %>%
+    dplyr::select("ConnectedTableName", "TableName.1", "TableName.2") %>%
+    tidyr::pivot_longer(cols = "TableName.1":"TableName.2", names_to = NULL, values_to = "TableName")
+
+  # add unconnected tables
+  df2 <- data_types %>%
+    dplyr::select("TableName") %>%
+    dplyr::filter(!(.data$TableName %in% df$TableName)) %>%
+    dplyr::mutate(ConnectedTableName = .data$TableName)
+  df <- dplyr::bind_rows(df, df2)
+
+  # construct the graph
+  data_graph <- igraph::graph_from_data_frame(df, directed = FALSE)
+
+  if(tables) {
+    return(list("data_graph" = data_graph,
+                "data_types" = data_types,
+                "connected_data_types" = data_connections))
+  } else {
+    return(data_graph)
+  }
+}
