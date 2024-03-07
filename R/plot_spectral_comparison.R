950ab82e1ff0b14e26a02807cd10be46a166e63f
jir88 <john.robinson@wustl.edu>
2024-03-07 12:10:58 -0600

    Add function plot_spectral_comparison for plotting two spectra back-to-back and marking matching fragments.

---
+++ R/plot_spectral_comparison.R
@@ -0,0 +1,71 @@
+#' Plot two mass spectra
+#'
+#' Plots two mass spectra back-to-back for comparison. Fragments within a
+#' given PPM tolerance of each other are marked.
+#'
+#' @param spec1 Spectrum to plot on top half of graph. Must have columns named
+#'   'mz' and 'intensity'.
+#' @param spec2 Spectrum to plot on bottom half of graph. Must have columns named
+#'   'mz' and 'intensity'.
+#' @param match_tol_ppm Fragment matching tolerance in PPM
+#'
+#' @return The ggplot object for rendering or further modification.
+#'
+#' @importFrom rlang .data
+#' @export
+plot_spectral_comparison <- function(spec1, spec2, match_tol_ppm = 5) {
+  spec1 <- as.matrix(spec1)
+  spec2 <- as.matrix(spec2)
+
+  # look for matching peaks
+  tol_low <- 1.0 - match_tol_ppm/1e6
+  tol_high <- 1.0 + match_tol_ppm/1e6
+
+  # s1_lower <- spec1[, "mz"]*tol_low
+  # s1_upper <- spec1[, "mz"]*tol_high
+
+  # sort spectrum 2 if it wasn't already
+  spec2 <- spec2[order(spec2[, "mz"]), ]
+
+  # locate fragment indices all at once
+  s2_idx_low <- findInterval(spec1[, "mz"]*tol_low,
+                             spec2[, "mz"])
+  s2_idx_high <- findInterval(spec1[, "mz"]*tol_high,
+                              spec2[, "mz"])
+
+  # which fragments' tolerance ranges in spec1 intersect 1+ fragments in spec2?
+  s1_matches <- (s2_idx_high - s2_idx_low) > 0
+  # indices of matching spec1 fragments
+  s1_match_idx <- which(s1_matches)
+
+  # which spec2 fragments match spec1 fragments?
+  s2_matches <- logical(length = nrow(spec2))
+  # for each spec1 fragment that matches 1+ spec2 fragment(s)
+  for(i in s1_match_idx) {
+    # get indices of spec2 fragments
+    idx <- seq.int(from = s2_idx_low[i] + 1,
+                   to = s2_idx_high[i])
+    # mark matches
+    s2_matches[idx] <- TRUE
+  }
+
+  # put spectrum 2 on the lower half
+  spec2[, "intensity"] <- -spec2[, "intensity"]
+  # combine spectra
+  combo_spec <- tibble::as_tibble(rbind(spec1, spec2))
+  # add marker
+  # combo_spec$Spectrum = rep(c("Spec1", "Spec2"), times = c(nrow(spec1), nrow(spec2)))
+  # mark matching fragments
+  combo_spec$Matches = c(s1_matches, s2_matches)
+
+  # return the plot
+  g <- ggplot2::ggplot(combo_spec, ggplot2::aes(x = .data[["mz"]],
+                                                xend = .data[["mz"]],
+                                                y = .data[["intensity"]],
+                                                yend = 0,
+                                                color = .data[["Matches"]])) +
+    ggplot2::geom_hline(yintercept = 0, color = "darkgrey") +
+    ggplot2::geom_segment() +
+    ggplot2::scale_color_manual(values = c("FALSE" = "black", "TRUE" = "blue"))
+  return(g)
+}
