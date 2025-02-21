efd5f260565ae4bfd6943d693736d64201a7d12e
jir88 <6749336+jir88@users.noreply.github.com>
2025-02-21 12:36:50 -0600

    Fix and speed up match_ms_features by using base R.

--- R/match_ms_features.R
+++ R/match_ms_features.R
@@ -40,17 +40,32 @@ match_ms_features <- function(mz1, rt1, mz2, rt2, id1 = NULL, id2 = NULL,
   colnames(delta_mass) <- id2
   rownames(delta_mass) <- id1
 
-  feature_comparison <- tibble::as_tibble(delta_mass, rownames = "Feature1",
-                                          .name_repair = "minimal") %>%
-    tidyr::pivot_longer(cols = -.data$Feature1, names_to = "Feature2", values_to = "ppm") %>%
-    dplyr::filter(dplyr::between(.data$ppm, ppm_range[1], ppm_range[2])) %>%
-    dplyr::mutate(mz1 = mz1[match(.data$Feature1, rownames(delta_mass))],
-                  rt1 = rt1[match(.data$Feature1, rownames(delta_mass))]) %>%
-    dplyr::mutate(mz2 = mz2[match(.data$Feature2, colnames(delta_mass))],
-                  rt2 = rt2[match(.data$Feature2, colnames(delta_mass))]) %>%
-    dplyr::mutate(dRT = rt1 - rt2) %>%
-    dplyr::filter(dplyr::between(.data$dRT, drt_range[1], drt_range[2])) %>%
-    dplyr::relocate(.data$ppm, .before = .data$dRT)
+  mz_matches <- which(delta_mass >= ppm_range[1] & delta_mass <= ppm_range[2], arr.ind = TRUE)
+
+  # extract all the near mz matches
+  feature_comparison <- tibble::tibble(Feature1 = id1[mz_matches[, 'row']],
+                       Feature2 = id2[mz_matches[, 'col']],
+                       mz1 = mz1[mz_matches[, 'row']],
+                       mz2 = mz2[mz_matches[, 'col']],
+                       rt1 = rt1[mz_matches[, 'row']],
+                       rt2 = rt2[mz_matches[, 'col']],
+                       ppm = delta_mass[mz_matches])
+  # filter for RT matches
+  feature_comparison$dRT <- feature_comparison$rt1 - feature_comparison$rt2
+  rt_matches <- which(feature_comparison$dRT >= drt_range[1] & feature_comparison$dRT <= drt_range[2])
+  feature_comparison <- feature_comparison[rt_matches, ]
+
+  # feature_comparison <- tibble::as_tibble(delta_mass, rownames = "Feature1",
+  #                                         .name_repair = "minimal") %>%
+  #   tidyr::pivot_longer(cols = -.data$Feature1, names_to = "Feature2", values_to = "ppm") %>%
+  #   dplyr::filter(dplyr::between(.data$ppm, ppm_range[1], ppm_range[2])) %>%
+  #   dplyr::mutate(mz1 = mz1[match(.data$Feature1, rownames(delta_mass))],
+  #                 rt1 = rt1[match(.data$Feature1, rownames(delta_mass))]) %>%
+  #   dplyr::mutate(mz2 = mz2[match(.data$Feature2, colnames(delta_mass))],
+  #                 rt2 = rt2[match(.data$Feature2, colnames(delta_mass))]) %>%
+  #   dplyr::mutate(dRT = rt1 - rt2) %>%
+  #   dplyr::filter(dplyr::between(.data$dRT, drt_range[1], drt_range[2])) %>%
+  #   dplyr::relocate(.data$ppm, .before = .data$dRT)
 
   return(feature_comparison)
 }
